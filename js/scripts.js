c1458d06df63a09e510ad8d8d17e0d3020d8b8d8
WhendelMorais <whendel.morais.9@gmail.com>
2019-10-20 18:10:36 -0300

    alterações no codigo javaScript

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 $(function(){
     $('nav.mobile').click(function(){
         var listaMenu = $('nav.mobile ul');
-        if(listaMenu.is(':hidden') == true)
-            listaMenu.fadeIn();
-        else
-            listaMenu.fadeOut();
+        if(listaMenu.is(':hidden') == true){
+            var icone = $('.botao-menu-mobile').find('i');
+            icone.removeClass('fas far-bars');
+            icone.addClass('fas fa-times');
+            listaMenu.slideToggle();
+        }
+        else{
+            var icone = $('.botao-menu-mobile').find('i');
+            icone.removeClass('fas fa-times');
+            icone.addClass('fas far-bars');
+            listaMenu.slideToggle();
+        }
     })
 })
